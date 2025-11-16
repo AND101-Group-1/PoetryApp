1e5733877545b33c069de0994a7b0ff05ea64f81
PrestBlake25 <prestblake257@gmail.com>
2025-11-15 23:15:55 -0500

    Initial commit

--- app/src/main/java/com/example/poe/Poem.kt
+++ app/src/main/java/com/example/poe/Poem.kt
@@ -3,6 +3,5 @@ package com.example.poe
 /** Poem class. Contains data for each poem. Title, author, lines, and line count. **/
 data class Poem(
     val title: String,
-    val author: String,
-    val lines: List<String>
+    val author: String
 )
